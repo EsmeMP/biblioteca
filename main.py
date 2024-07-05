ba1132ea7368f0690d2eee70eda5c62e96ed9a7c
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-07-05 10:43:15 -0600

    nuevo-enlaceBD

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import psycopg2
 from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import PaawordField, StringField, SubmitField
 
 app = Flask (__name__)
 bootstrap = Bootstrap(app)
@@ -8,3 +10,23 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('base.html')
+
+@app.route('libros')
+def libros():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="2580",
+        host="localhost",
+        port="5432"
+    )
+    # Crar un cursor para recorrer el contenido de las tablas
+    cursor = conexion.cursor()
+    # ejecutar consulta en postgres, informacion de libros
+    cursor.execute('''SELECT * FROM libros''')
+    datos = cursor.fetchall()
+    # cerrar conexion a la bd
+    cursor.close()
+    conexion.close()
+    return render_template('libros.html', datos=datos)
