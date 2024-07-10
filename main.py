f049f7c459ac80bd4d0d9380d2b03dcae1fced52
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-07-10 12:59:15 -0600

    Autores

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
+# import psycopg2
+# from flask import Flask, redirect, render_template
+# from flask_bootstrap import Bootstrap
+# from flask_wtf import FlaskForm
+# from wtforms.fields import PasswordField, StringField, SubmitField
+
+# app = Flask (__name__)
+# bootstrap = Bootstrap(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('base.html')
+
+# @app.route('libros')
+# def libros():
+#     # Conectar con la base de datos
+#     conexion = psycopg2.connect(
+#         database="biblioteca3a",
+#         user="postgres",
+#         password="2580",
+#         host="localhost",
+#         port="5432"
+#     )
+#     # Crar un cursor para recorrer el contenido de las tablas
+#     cursor = conexion.cursor()
+#     # ejecutar consulta en postgres, informacion de libros
+#     cursor.execute('''SELECT * FROM libros''')
+#     datos = cursor.fetchall()
+#     # cerrar conexion a la bd
+#     cursor.close()
+#     conexion.close()
+#     return render_template('libros.html', datos=datos)
+
 import psycopg2
 from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms.fields import PaawordField, StringField, SubmitField
+from wtforms.fields import PasswordField, StringField, SubmitField
 
-app = Flask (__name__)
+
+app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
-@app.route('libros')
+@app.route('/libros')
 def libros():
     # Conectar con la base de datos
     conexion = psycopg2.connect(
@@ -21,12 +55,36 @@ def libros():
         host="localhost",
         port="5432"
     )
-    # Crar un cursor para recorrer el contenido de las tablas
+    # crear un cursor (objeto para recorrer las tablas)
     cursor = conexion.cursor()
-    # ejecutar consulta en postgres, informacion de libros
-    cursor.execute('''SELECT * FROM libros''')
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM libros_view''')
+    #recuperar la informacion
     datos = cursor.fetchall()
-    # cerrar conexion a la bd
+    #cerrar cursos y conexion a la base de datos
     cursor.close()
     conexion.close()
     return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+    # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="2580",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    # Primer cambio
+    cursor.execute('''SELECT * FROM autores_view''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    # Primer cambio
+    return render_template('autores.html', datos=datos)
