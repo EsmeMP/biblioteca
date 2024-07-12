b06656b6b4548d10cc65ae12c56a61110468c119
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-07-12 10:49:01 -0600

    Agregando delete en Pais

--- main.py
+++ main.py
@@ -32,7 +32,7 @@
 #     return render_template('libros.html', datos=datos)
 
 import psycopg2
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import PasswordField, StringField, SubmitField
@@ -88,3 +88,43 @@ def autores():
     conexion.close()
     # Primer cambio
     return render_template('autores.html', datos=datos)
+
+@app.route('/paises')
+def paises():
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="2580",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM pais''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('paises.html', datos=datos)
+
+# Eliminar Pais
+@app.route('/delete_pais/<int:id_pais>', methods= ['POST'])
+def delete_pais(id_pais):
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="2580",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # Borrar el registro con el id_seleccionado
+    cursor.execute('''DELETE FROM pais WHERE id_pais= %s''',
+                   (id_pais,))
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    return redirect(url_for('index'))
